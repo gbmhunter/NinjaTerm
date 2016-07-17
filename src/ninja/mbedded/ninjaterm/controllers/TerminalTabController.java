f16df02ac59c742d8c3349d36a72cd77226bd4ad
Geoffrey Hunter <Geoffrey Hunter>
2016-07-17 15:24:59 +1200

    Auto-scroll functionality can now be enabled/disabled, and the auto-scroll button hides itself at the apporpriate times.

--- src/ninja/mbedded/ninjaterm/controllers/TerminalTabController.java
+++ src/ninja/mbedded/ninjaterm/controllers/TerminalTabController.java
@@ -1,17 +1,15 @@
 package ninja.mbedded.ninjaterm.controllers;
 
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
-import jssc.SerialPortList;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,9 +17,9 @@ import java.util.ResourceBundle;
 /**
  * Controller for the "Terminal" tab which is part of the main window.
  *
- * @author          Geoffrey Hunter <gbmhunter@gmail.com> (www.mbedded.ninja)
- * @since           2016-07-16
- * @last-modified   2016-07-17
+ * @author Geoffrey Hunter <gbmhunter@gmail.com> (www.mbedded.ninja)
+ * @last-modified 2016-07-17
+ * @since 2016-07-16
  */
 public class TerminalTabController implements Initializable {
 
@@ -36,17 +34,24 @@ public class TerminalTabController implements Initializable {
     public TextFlow rxTextTextFlow;
 
     @FXML
+    public Pane autoScrollButtonPane;
+
+    @FXML
     public ImageView scrollToBottomImageView;
 
     //================================================================================================//
     //=========================================== CLASS FIELDS =======================================//
     //================================================================================================//
 
+    private static final double AUTO_SCROLL_BUTTON_OPACITY_NON_HOVER = 0.35;
+
+    private static final double AUTO_SCROLL_BUTTON_OPACITY_HOVER = 1.0;
+
     /**
      * Determines whether the RX data terminal will auto-scroll to bottom
      * as more data arrives.
      */
-    private Boolean stickToBottomEnabled = true;
+    private Boolean autoScrollEnabled = true;
 
     //================================================================================================//
     //========================================== CLASS METHODS =======================================//
@@ -54,6 +59,7 @@ public class TerminalTabController implements Initializable {
 
     /**
      * Called automatically by system when .fxml file is loaded.
+     *
      * @param location
      * @param resources
      */
@@ -64,31 +70,59 @@ public class TerminalTabController implements Initializable {
         // in scene builder)
         rxTextTextFlow.getChildren().clear();
 
+        // Hide the auto-scroll image-button, this will be made visible
+        // when the user manually scrolls
+        autoScrollButtonPane.setVisible(false);
+
+
+        scrollToBottomImageView.setOpacity(AUTO_SCROLL_BUTTON_OPACITY_NON_HOVER);
+
+        // This adds a listener which will implement the "auto-scroll" functionality
+        // when it is enabled with @link{autoScrollEnabled}.
         rxTextTextFlow.heightProperty().addListener((observable, oldValue, newValue) -> {
             System.out.println("heightProperty changed to " + newValue);
 
-            if(stickToBottomEnabled) {
+            if (autoScrollEnabled) {
                 rxDataScrollPane.setVvalue(rxTextTextFlow.getHeight());
             }
 
+        });
+
+        rxDataScrollPane.addEventFilter(ScrollEvent.ANY, event -> {
+
+            // Since the user has now scrolled manually, disable the
+            // auto-scroll
+            autoScrollEnabled = false;
+
+            autoScrollButtonPane.setVisible(true);
         });
 
         //==============================================//
         //=== SCROLL-TO-BOTTOM BUTTON EVENT HANDLERS ===//
         //==============================================//
 
-        scrollToBottomImageView.addEventFilter(MouseEvent.MOUSE_ENTERED, (MouseEvent mouseEvent) -> {
-                System.out.println("Mouse entered." + mouseEvent.getSource());
-            }
-        );
-
-        scrollToBottomImageView.addEventFilter(MouseEvent.MOUSE_EXITED, (MouseEvent mouseEvent) -> {
-                    System.out.println("Mouse exited." + mouseEvent.getSource());
+        autoScrollButtonPane.addEventFilter(MouseEvent.MOUSE_ENTERED, (MouseEvent mouseEvent) -> {             
+                    scrollToBottomImageView.setOpacity(AUTO_SCROLL_BUTTON_OPACITY_HOVER);
                 }
         );
 
-        scrollToBottomImageView.addEventFilter(MouseEvent.MOUSE_CLICKED, (MouseEvent mouseEvent) -> {
+        autoScrollButtonPane.addEventFilter(MouseEvent.MOUSE_EXITED, (MouseEvent mouseEvent) -> {
+                    scrollToBottomImageView.setOpacity(AUTO_SCROLL_BUTTON_OPACITY_NON_HOVER);
+                }
+        );
+
+        autoScrollButtonPane.addEventFilter(MouseEvent.MOUSE_CLICKED, (MouseEvent mouseEvent) -> {
                     System.out.println("Mouse click detected! " + mouseEvent.getSource());
+
+                    // Enable auto-scroll
+                    autoScrollEnabled = true;
+
+                    autoScrollButtonPane.setVisible(false);
+
+                    // Manually perform one auto-scroll, since the next automatic one won't happen until
+                    // the height of rxTextTextFlow changes.
+                    rxDataScrollPane.setVvalue(rxTextTextFlow.getHeight());
+
                 }
         );
 
@@ -96,7 +130,8 @@ public class TerminalTabController implements Initializable {
 
     /**
      * Adds the given text to the RX terminal display.
-     * @param rxText    The text you want to add.
+     *
+     * @param rxText The text you want to add.
      */
     public void addRxText(String rxText) {
 
