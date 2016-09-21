6ede15abd26080304d029d0e4ab647125bbe3ac7
Geoffrey Hunter <gbmhunter@gmail.com>
2016-09-21 19:54:04 +0100

    Added delay between end of app info and stat of bogus messages on the splash screen.

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 
     private GlyphFont glyphFont;
 
-    private final boolean disableSplashScreen = true;
+    private final boolean disableSplashScreen = false;
 
     private Stage splashScreenStage;
     private Stage mainStage;
