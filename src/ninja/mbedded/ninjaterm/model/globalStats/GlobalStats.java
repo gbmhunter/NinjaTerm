56eda2ceb8f81bb30be18a4b837777394ee480cc
Geoffrey Hunter <gbmhunter@gmail.com>
2016-09-25 11:24:56 +0100

    Added comments to the global statistics page.

--- src/ninja/mbedded/ninjaterm/model/globalStats/GlobalStats.java
+++ src/ninja/mbedded/ninjaterm/model/globalStats/GlobalStats.java
@@ -3,11 +3,22 @@ package ninja.mbedded.ninjaterm.model.globalStats;
 import javafx.beans.property.SimpleIntegerProperty;
 
 /**
- * Created by gbmhu on 2016-09-16.
+ * Model containing data and logic global statistics (not related to just one terminal).
+ *
+ * @author          Geoffrey Hunter <gbmhunter@gmail.com> (www.mbedded.ninja)
+ * @since           2016-09-16
+ * @last-modified   2016-09-25
  */
 public class GlobalStats {
 
+    /**
+     * The total number of characters send to all of the COM port.
+     */
     public SimpleIntegerProperty numCharactersTx = new SimpleIntegerProperty(0);
+
+    /**
+     * The total number of characters received form all of the COM ports.
+     */
     public SimpleIntegerProperty numCharactersRx = new SimpleIntegerProperty(0);
 
 }
