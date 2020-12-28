3556d6c0d0009076d633f0717f03ebc49f4e8432
Geoffrey Hunter <gbmhunter@gmail.com>
2020-12-29 10:48:58 +1300

    Added comment to eslint settings.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,10 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     'prettier/prettier': 'off', // Disable all the prettier rules, these are a pain in the butt
+     // The application model relies on circular imports, i.e. App uses Settings and Settings uses App.
+     // I should probably fix this in the future but at the moment the type awareness of the circular
+     // imports is useful
+    'import/no-cycle': 'off',
   },
   parserOptions: {
     ecmaVersion: 2020,
