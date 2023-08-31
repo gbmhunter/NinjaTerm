fa5404275d8cbdccb1b09999392c011f55be3068
Geoffrey Hunter <gbmhunter@gmail.com>
2023-08-31 20:17:52 +1200

    Added App test file.

---
+++ src/model/App.test.tsx
@@ -0,0 +1,15 @@
+/**
+ * Unit tests for the AnsiECParser class.
+ *
+ * @author          Geoffrey Hunter <gbmhunter@gmail.com> (www.mbedded.ninja)
+ * @since           2016-09-26
+ */
+
+import { AppStore } from './App';
+
+describe('App tests', () => {
+  it('can parse single sequence', () => {
+    const app = new AppStore();
+    expect(true).toEqual(true);
+  });
+});
