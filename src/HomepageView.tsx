d25e45eb4225b56ccd159d4ff3800f505469b627
Geoffrey Hunter <gbmhunter@gmail.com>
2023-09-30 11:40:18 +1300

    Added cursor animation to landing page.

--- src/HomepageView.tsx
+++ src/HomepageView.tsx
@@ -1,25 +1,29 @@
-import { observer } from 'mobx-react-lite';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { Box, Button } from '@mui/material';
+import { observer } from "mobx-react-lite";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Box, Button, Grid, Typography } from "@mui/material";
+import TerminalIcon from "@mui/icons-material/Terminal";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
-import GitHubReadmeLogo from './github-readme-logo.png';
+import GitHubReadmeLogo from "./github-readme-logo.png";
+
+import "./HomepageView.css"
 
 // Create dark theme for MUI
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
     background: {
-      default: '#202020',
-      paper: '#202020',
+      default: "#202020",
+      paper: "#202020",
       // paper: deepOrange[900],
     },
     primary: {
       // main: '#dc3545', // your primary color
-      main: '#66D14F', // your primary color
+      main: "#E47F37", // your primary color
     },
     secondary: {
-      main: '#35dccb', // your secondary color
+      main: "#35dccb", // your secondary color
     },
   },
   typography: {
@@ -32,38 +36,77 @@ const darkTheme = createTheme({
         tooltip: {
           // Override default font size for all tool-tips, as default is a little
           // to small
-          fontSize: '0.8rem',
+          fontSize: "0.8rem",
         },
       },
     },
   },
 });
 
-
-interface Props {
-}
+interface Props {}
 
 export default observer((props: Props) => {
-
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-        <Box sx={{
-          boxSizing: 'border-box',
+      <Box
+        sx={{
+          boxSizing: "border-box",
           // backgroundColor: '#000000',
-          height: '100%',
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center' }}
-        >
-        <Box sx={{ height: '20px' }} />
-        <img src={GitHubReadmeLogo} alt="NinjaTerm logo." width="600px" />
-        <Box sx={{ height: '20px' }} />
-        <Button href='/app/' variant='contained' size='large'>Go to app</Button>
-        <Box sx={{ height: '20px' }} />
-        <p>A serial port terminal that's got your back.</p>
-    </Box>
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Grid>
+          <Box sx={{ height: "20px" }} />
+          <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+            <img src={GitHubReadmeLogo} alt="NinjaTerm logo." width="600px" />
+          </Box>
+          <Box sx={{ height: "20px" }} />
+          <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+          <span style={{ fontFamily: 'monospace', fontSize: '30px' }}>
+            A serial port terminal that's got your back.<span className="cursor">&nbsp;</span>
+          </span>
+          </Box>
+          <Box sx={{ height: "20px" }} />
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              gap: "20px",
+            }}
+          >
+            <Button
+              href="/app/"
+              variant="contained"
+              size="large"
+              startIcon={<TerminalIcon />}
+            >
+              Go to app
+            </Button>
+            <Button
+              href="https://github.com/gbmhunter/NinjaTerm"
+              target="_blank"
+              variant="outlined"
+              size="large"
+              startIcon={<GitHubIcon />}
+            >
+              GitHub
+            </Button>
+          </Box>
+          <Box sx={{ height: "20px" }} />
+
+          <p>
+            NinjaTerm is a serial port terminal designed for viewing debug
+            serial port data when writing firmware for a microcontroller.
+          </p>
+
+          <p>ANSI Escape Sequence Support</p>
+        </Grid>
+      </Box>
     </ThemeProvider>
   );
 });
