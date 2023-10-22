861a1b642e80bb88d734df3cf21e27bf6c303c52
Geoffrey Hunter <gbmhunter@gmail.com>
2023-10-23 09:15:48 +1300

    Modified example Playwright tests.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,13 @@
+/* eslint-disable testing-library/prefer-screen-queries */
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('Basic loading', () => {
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  test('has title', async ({ page }) => {
+    await page.goto('/');
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+    // Expect the "Go to app" button to be visible on the homepage.
+    await page.getByText(/Go to app/).click();
+  });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
