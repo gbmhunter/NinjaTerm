91ea4c4bd3894a1a918d66b11848d37cd59c780b
Geoffrey Hunter <gbmhunter@gmail.com>
2023-11-10 21:14:49 +1300

    Working on support for multiple configs.

--- src/Storage/AppStorage.ts
+++ src/Storage/AppStorage.ts
@@ -9,11 +9,20 @@ class Data {
   lastUsedSerialPort: LastUsedSerialPort = new LastUsedSerialPort();
 }
 
+class Config {
+  name: string = '';
+  configData: any = {};
+}
+
 export default class AppStorage {
 
   app: App;
 
   data: Data = new Data();
+
+  configs: Config[] = [];
+
+  activeConfig: Config;
 
   constructor(app: App) {
     this.app = app;
@@ -25,6 +34,19 @@ export default class AppStorage {
         this.data.lastUsedSerialPort = new LastUsedSerialPort();
       }
     }
+
+    // Read in configurations
+    const configsStr = window.localStorage.getItem('configs');
+    if (configsStr === null) {
+      const defaultConfig = new Config();
+      this.configs.push(defaultConfig);
+    } else {
+      this.configs = JSON.parse(configsStr);
+    }
+    // Only support the 1 active config for now
+    this.activeConfig = this.configs[0];
+
+    this.saveConfig2(['test1', 'test2'], 'hello');
   }
 
   saveData = () => {
@@ -42,5 +64,27 @@ export default class AppStorage {
     }
     return null;
   }
+
+  saveConfig2(keys: string[], data: any) {
+    console.log('saveConfig() called with keys:', keys, 'and data:', data);
+    let obj = this.activeConfig.configData;
+    // Walk down the active config object using
+    // the array of keys
+    for (let i = 0; i < keys.length - 1; i++) {
+      const key = keys[i];
+      console.log('key:', key);
+      // Make sure key exists, if not
+      // create it
+      if (obj[key] === undefined) {
+        obj[key] = {};
+      }
+      obj = obj[key];
+    }
+    obj[keys[keys.length - 1]] = data;
+    console.log('configData:', JSON.stringify(this.activeConfig.configData));
+    // window.localStorage.setItem(key, JSON.stringify(data));
+  }
+
+
 
 }
