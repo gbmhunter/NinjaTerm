fa0ab3e08bd14cc7ab409d77c93beee68b7880f8
Geoffrey Hunter <gbmhunter@gmail.com>
2024-05-25 20:36:17 +1200

    Made profile selection controlled.

--- src/model/Settings/ProfilesSettings/ProfilesSettings.ts
+++ src/model/Settings/ProfilesSettings/ProfilesSettings.ts
@@ -1,13 +1,24 @@
+import { GridRowSelectionModel } from "@mui/x-data-grid";
 import { makeAutoObservable } from "mobx";
 
 import AppStorage from "src/model/Storage/AppStorage";
 import { createSerializableObjectFromConfig, updateConfigFromSerializable } from "src/model/Util/SettingsLoader";
+import { ApplyableNumberField, ApplyableTextField } from "src/view/Components/ApplyableTextField";
+import { z } from "zod";
 
 
 export default class ProfilesSettings {
 
+  profileName = new ApplyableTextField(" ", z.string());
+
+  selectedProfiles: GridRowSelectionModel = [];
+
   constructor() {
     makeAutoObservable(this); // Make sure this is at the end of the constructor
+  }
+
+  setSelectedProfiles(selectedProfiles: GridRowSelectionModel) {
+    this.selectedProfiles = selectedProfiles;
   }
 
 }
--- src/view/Settings/ProfileSettings/ProfileSettingsView.tsx
+++ src/view/Settings/ProfileSettings/ProfileSettingsView.tsx
@@ -1,6 +1,6 @@
 import { Button, Checkbox, FormControl, FormControlLabel, InputLabel, Select, Tooltip } from "@mui/material";
 import { observer } from "mobx-react-lite";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { DataGrid, GridColDef, GridRowSelectionModel } from "@mui/x-data-grid";
 
 import ProfilesSettings from "src/model/Settings/ProfilesSettings/ProfilesSettings";
 import AppStorage from "src/model/Storage/AppStorage";
@@ -9,6 +9,7 @@ import BorderedSection from "src/view/Components/BorderedSection";
 import { DataType } from "src/model/Settings/RxSettings/RxSettings";
 import PortConfiguration, { PortConfigurationConfig } from "src/model/Settings/PortConfigurationSettings/PortConfigurationSettings";
 import { ProfileManager } from "src/model/ProfileManager/ProfileManager";
+import ApplyableTextFieldView from "src/view/Components/ApplyableTextFieldView";
 
 interface Props {
   profileManager: ProfileManager;
@@ -41,30 +42,7 @@ function ProfileSettingsView(props: Props) {
 
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "start" }}>
-      {/* <FormControl sx={{ m: 1, minWidth: 300, maxWidth: 300 }}>
-        <InputLabel shrink htmlFor="select-multiple-native">
-          Profiles
-        </InputLabel>
-        <Select<string[]>
-          native
-          multiple
-          // value={personName}
-          // @ts-ignore Typings are not considering `native`
-          // onChange={handleChangeMultiple}
-          label="Native"
-          inputProps={{
-            id: "select-multiple-native",
-          }}
-        >
-          {profiles.map((profile) => (
-            <option key={profile.name} value={profile.name}>
-              {profile.name}
-            </option>
-          ))}
-        </Select>
-      </FormControl> */}
-
-      <div style={{ height: 400, width: "100%" }}>
+      <div style={{ height: 400, width: 600 }}>
         <DataGrid
           rows={rows}
           columns={columns}
@@ -74,7 +52,15 @@ function ProfileSettingsView(props: Props) {
             },
           }}
           pageSizeOptions={[5, 10]}
-          checkboxSelection
+          density="compact"
+          rowSelectionModel={profilesSettings.selectedProfiles}
+          onRowSelectionModelChange={(newRowSelectionModel: GridRowSelectionModel) => {
+            console.log('sss', newRowSelectionModel);
+            profilesSettings.setSelectedProfiles(newRowSelectionModel);
+            // setRowSelectionModel(newRowSelectionModel);
+          }}
+          // checkboxSelection
+          // disableMultipleRowSelection={true}
           // Hide the "select all" checkbox in the header row
           sx={{
             "& .MuiDataGrid-columnHeaderCheckbox .MuiDataGrid-columnHeaderTitleContainer": {
@@ -107,6 +93,26 @@ function ProfileSettingsView(props: Props) {
       >
         Delete Profile
       </Button>
+
+      <div style={{ height: 20 }} />
+
+      {/* =============================================================================== */}
+      {/* SCROLLBACK BUFFER SIZE */}
+      {/* =============================================================================== */}
+      <Tooltip
+        title="The max. number of rows to store in any terminal scrollback buffer (TX, RX, TX/RX).
+        Increasing this will give you more history but decrease performance and increase memory usage. Must be a positive non-zero integer."
+        followCursor
+        arrow
+      >
+        <ApplyableTextFieldView
+          label="Profile name"
+          variant="outlined"
+          size="small"
+          applyableTextField={profilesSettings.profileName}
+          sx={{ marginBottom: "20px" }}
+        />
+      </Tooltip>
     </div>
   );
 }
